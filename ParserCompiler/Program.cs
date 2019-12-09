709603c7d1707a9a3da94d782983542d83ec2784
zoran-horvat <zh@codinghelmet.com>
2019-12-10 00:03:54 +0100

    Added trivial text template

---
+++ ParserCompiler/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ParserCompiler
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var x = new ParsingTable();
+            Console.WriteLine(x);
+            Console.ReadLine();
+        }
+    }
+}
