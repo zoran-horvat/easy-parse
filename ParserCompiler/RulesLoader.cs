b6f5c2fbb13c5af356f8e3a546f839ed2382eec8
zoran-horvat <zh@codinghelmet.com>
2019-12-10 00:51:48 +0100

    Loading grammar rules using a class

---
+++ ParserCompiler/RulesLoader.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace ParserCompiler
+{
+    public class RulesLoader
+    {
+        private IServiceProvider ServiceProvider { get; }
+
+        public RulesLoader(IServiceProvider serviceProvider)
+        {
+            this.ServiceProvider = serviceProvider;
+        }
+
+        public string[] LoadFrom(string fileName) =>
+            this.GrammarItem(fileName).FileNames[0] is string filePath && File.Exists(filePath) ? File.ReadAllLines(filePath)
+            : new string[0];
+
+        private EnvDTE.ProjectItem GrammarItem(string fileName) =>
+            this.VisualStudio.Solution.FindProjectItem(fileName);
+
+        private EnvDTE.DTE VisualStudio =>
+            this.ServiceProvider.GetService(typeof(EnvDTE.DTE)) as EnvDTE.DTE;
+    }
+}
