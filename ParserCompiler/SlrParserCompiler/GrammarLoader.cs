694ec0537d5164e178bf2e1ab9802d4e5d8b99fc
zoran-horvat <zh@codinghelmet.com>
2019-12-11 22:43:16 +0100

    Decoupled the object model from Visual Studio
    Dependency on Visual Studio remains in the T4 template only. Object model is just working with plain files.

--- ParserCompiler/SlrParserCompiler/GrammarLoader.cs
+++ ParserCompiler/SlrParserCompiler/GrammarLoader.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -7,27 +6,10 @@ namespace ParserCompiler.SlrParserCompiler
 {
     public class GrammarLoader
     {
-        private IServiceProvider ServiceProvider { get; }
-
-        public GrammarLoader(IServiceProvider serviceProvider)
-        {
-            this.ServiceProvider = serviceProvider;
-        }
-
-        public IEnumerable<string> From(string fileName) =>
-            this.RawLines(fileName)
+        public IEnumerable<string> From(string filePath) =>
+            File.ReadAllLines(filePath)
                 .Where(line => !string.IsNullOrWhiteSpace(line))
                 .Select(line => line.Trim())
                 .Where(line => !line.StartsWith("#"));
-
-        private IEnumerable<string> RawLines(string fileName) =>
-            this.GrammarItem(fileName).FileNames[0] is string filePath && File.Exists(filePath) ? File.ReadAllLines(filePath)
-            : new string[0];
-
-        private EnvDTE.ProjectItem GrammarItem(string fileName) =>
-            this.VisualStudio.Solution.FindProjectItem(fileName);
-
-        private EnvDTE.DTE VisualStudio =>
-            this.ServiceProvider.GetService(typeof(EnvDTE.DTE)) as EnvDTE.DTE;
     }
 }
