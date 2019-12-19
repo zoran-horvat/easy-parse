c14cf7a47219d7189fb076f0a5e9efbed55c93ea
zoran-horvat <zh@codinghelmet.com>
2019-12-19 23:19:57 +0100

    Defined primitive lexing API

---
+++ EasyParse/LexicalAnalysis/Lexeme.cs
@@ -0,0 +1,14 @@
+﻿namespace EasyParse.LexicalAnalysis
+{
+    public class Lexeme
+    {
+        public string Label { get; }
+        public string Value { get; }
+
+        public Lexeme(string label, string value)
+        {
+            this.Label = label;
+            this.Value = value;
+        }
+    }
+}
---
+++ EasyParse/LexicalAnalysis/Lexer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace EasyParse.LexicalAnalysis
+{
+    public class Lexer
+    {
+        public Lexer AddPattern(string regex, string lexeme) => this;
+        public Lexer IgnorePattern(string regex) => this;
+
+        public IEnumerable<Lexeme> Tokenize(string input) => Enumerable.Empty<Lexeme>();
+    }
+}
