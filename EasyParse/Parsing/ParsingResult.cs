e27315946be3bcb5a368f45a547b1310c8cf908c
zoran-horvat <zh@codinghelmet.com>
2019-12-24 00:22:34 +0100

    Defined primitive transformation which serves as a compiler
    Transform is specified as a Func delegate and it transofrms a given node into an arbitrary object. Transforms are applied to syntax tree nodes in postorder.

--- EasyParse/Parsing/ParsingResult.cs
+++ EasyParse/Parsing/ParsingResult.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using EasyParse.Parsing.Formatting;
 using EasyParse.Parsing.Nodes;
 
@@ -13,6 +14,28 @@ namespace EasyParse.Parsing
             this.Content = content;
         }
 
-        public override string ToString() => this.Content.Printable();
+        public string ErrorMessage =>
+            this.Content is Error error ? error.Message : string.Empty;
+
+        public bool IsSuccess =>
+            this.Content is Node;
+
+        public object Compile(Func<string, object[], object> nodeCompiler) =>
+            this.Content is Node node ? this.Compile(node, nodeCompiler)
+            : throw new InvalidOperationException("Cannot compile failed parse result.");
+
+        private object Compile(Node node, Func<string, object[], object> nodeCompiler) =>
+            node is TerminalNode terminal ? this.Compile(terminal, nodeCompiler)
+            : node is NonTerminalNode nonTerminal ? this.Compile(nonTerminal, nodeCompiler)
+            : throw new InvalidOperationException($"Internal error compiling {node}");
+
+        private object Compile(TerminalNode terminal, Func<string, object[], object> nodeCompiler) =>
+            nodeCompiler(terminal.Label, new object[] {terminal.Value});
+
+        private object Compile(NonTerminalNode nonTerminal, Func<string, object[], object> nodeCompiler) =>
+            nodeCompiler(nonTerminal.Label, nonTerminal.Children.Select(child => this.Compile(child, nodeCompiler)).ToArray());
+
+        public override string ToString() => 
+            this.Content.Printable();
     }
 }
--- ParserCompiler.TextGenerationDemo/Program.cs
+++ ParserCompiler.TextGenerationDemo/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using EasyParse.LexicalAnalysis.Tokens;
 using EasyParse.Parsing;
-using EasyParse.Parsing.Nodes;
 
 namespace ParserCompiler.TextGenerationDemo
 {
@@ -24,9 +23,43 @@ namespace ParserCompiler.TextGenerationDemo
         {
             List<Token> tokens = parser.Lexer.Tokenize(line).ToList();
             string tokensReport = string.Join(" ", tokens.Select(x => $"{x}"));
+            
+            Console.WriteLine();
             Console.WriteLine($"Tokens: {tokensReport}");
+            
             ParsingResult result = parser.Parse(line);
-            Console.WriteLine($"Result:{Environment.NewLine}{result}");
+            Console.WriteLine();
+            Console.WriteLine($"Syntax tree:{Environment.NewLine}{result}");
+
+            Console.WriteLine();
+            Console.WriteLine($"{line} = {(result.IsSuccess ? result.Compile(Calculator) : result.ErrorMessage)}");
+            Console.WriteLine(new string('-', 50));
         }
+
+        private static object Calculator(string label, object[] arguments) =>
+            label == "n" && int.TryParse((string) arguments[0], out int number) ? number
+            : label == "n" ? "<Overflow>"
+            : label == "A" && arguments.Length == 1 ? arguments[0]
+            : label == "A" && arguments[0] is string s1 ? s1
+            : label == "A" && arguments[2] is string s2 ? s2
+            : label == "A" && arguments[1].Equals("+") ? Add((int) arguments[0], (int) arguments[2])
+            : label == "A" && arguments[1].Equals("-") ? Subtract((int) arguments[0], (int) arguments[2])
+            : label == "E" ? arguments[0]
+            : arguments.Length == 1 ? arguments[0]
+            : "<Internal error>";
+
+        private static object Add(int a, int b) =>
+            a < 0 && b > 0 ? a + b
+            : a > 0 && b < 0 ? a + b
+            : a < 0 && b < 0 && int.MinValue - a <= b ? a + b
+            : int.MaxValue - a >= b ? (object)(a + b)
+            : "<Overflow>";
+
+        private static object Subtract(int a, int b) =>
+            a > 0 && b > 0 ? a - b
+            : a < 0 && b < 0 ? a - b
+            : a < 0 && b > 0 && int.MinValue + b <= a ? a - b
+            : int.MinValue - a <= b ? (object) (a - b)
+            : "<Overflow>";
     }
 }
