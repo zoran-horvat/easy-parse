384b1d9322d4ef7cbc9a255f46a4898ce22b9097
zoran-horvat <zh@codinghelmet.com>
2019-12-27 23:21:15 +0100

    Defined basic infrastructure for the grammar parser

---
+++ EasyParse.Tests/GrammarGrammarTests.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using EasyParse.LexicalAnalysis;
+using EasyParse.ParserGenerator;
+using EasyParse.Testing;
+
+namespace EasyParse.Tests
+{
+    public class GrammarGrammarTests : ParserTestsBase
+    {
+        protected override Assembly XmlDefinitionAssembly => typeof(GrammarLoader).Assembly;
+        protected override string XmlDefinitionResourceName => "EasyParse.ParserGenerator.GrammarParserDefinition.xml";
+        protected override Lexer Lexer => new Lexer();
+    }
+}
