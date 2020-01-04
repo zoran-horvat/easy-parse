380e21e3ebaa059e7d22032cc0cc7fde25210571
zoran-horvat <zh@codinghelmet.com>
2020-01-04 01:38:46 +0100

    Introduced base compiler class
    Abstract compiler holds a map between symbols (terminals and nonterminals) and method names. Then it attempts to match strongly typed child nodes of any nonterminal into strongly typed method arguments. If successful, the compiler invokes the method to build another compiled node. The process continues either until some node fails to compile (by not being able to bind a concrete method that would compile it), or when the entire syntax tree is compield into the final result.

--- EasyParse/ParserGenerator/GrammarCompiler/Compiler.cs
+++ EasyParse/ParserGenerator/GrammarCompiler/Compiler.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.Collections.Immutable;
-using System.Linq;
 using System.Reflection;
 using EasyParse.ParserGenerator.Models.Rules;
 using EasyParse.ParserGenerator.Models.Symbols;
@@ -8,7 +7,7 @@ using EasyParse.Parsing;
 
 namespace EasyParse.ParserGenerator.GrammarCompiler
 {
-    public class Compiler : ICompiler
+    public class Compiler : MethodMapCompiler
     {
         private Parser StringParser { get; } = Parser.FromXmlResource(
             Assembly.GetExecutingAssembly(), 
@@ -17,75 +16,44 @@ namespace EasyParse.ParserGenerator.GrammarCompiler
 
         private StringCompiler StringCompiler { get; } = new StringCompiler();
 
-        public object CompileTerminal(string label, string value)
+        protected override IEnumerable<(string label, string methodName)> Map => new[]
         {
-            var x = new[] {"t", "l", "i", "s"}.Contains(label) ? new Terminal(value)
-                : label == "q" ? new Terminal(this.CompileStringLiteral(value))
-                : label == "n" ? (object) new NonTerminal(value)
-                : value;
-            return x;
-        }
+            ("r", nameof(Terminal)),
+            ("t", nameof(Terminal)),
+            ("l", nameof(Terminal)),
+            ("i", nameof(Terminal)),
+            ("s", nameof(Terminal)),
+            ("a", nameof(Terminal)),
+            ("e", nameof(Terminal)),
+            ("q", nameof(QuotedString)),
+            ("n", nameof(NonTerminal)),
+            ("F", nameof(FullGrammar)),
+            ("L", nameof(Lexemes)),
+            ("P", nameof(LexemePattern)),
+            ("G", nameof(Grammar)),
+            ("R", nameof(Rule)),
+            ("B", nameof(RuleBody)),
+            ("S", nameof(Symbol))
+        };
+
+        private Terminal Terminal(string value) => new Terminal(value);
+        private Terminal QuotedString(string raw) => new Terminal(this.CompileStringLiteral(raw));
+        private NonTerminal NonTerminal(string value) => new NonTerminal(value);
+        private Grammar FullGrammar(ImmutableList<Lexeme> lexemes, Terminal rulesKeyword, Terminal endOfLine, Grammar grammar) => grammar.AddRange(lexemes);
+        private Grammar FullGrammar(Terminal endOfLine, Grammar grammar) => grammar;
+        private ImmutableList<Lexeme> Lexemes(Terminal lexemesKeyword, Terminal endOfLine) => ImmutableList<Lexeme>.Empty;
+        private ImmutableList<Lexeme> Lexemes(ImmutableList<Lexeme> lexemes, Lexeme next, Terminal endOfLine) => lexemes.Add(next);
+        private Lexeme LexemePattern(Terminal name, Terminal @is, Terminal pattern) => new LexemePattern(name.Value, pattern.Value);
+        private Lexeme LexemePattern(Terminal ignore, Terminal pattern) => new IgnoreLexeme(pattern.Value);
+        private Grammar Grammar(Rule rule, Terminal endOfLine) => new Grammar(rule);
+        private Grammar Grammar(Grammar rules, Rule next, Terminal endOfLine) => rules.Add(next);
+        private Rule Rule(NonTerminal nonTerminal, Terminal arrow, ImmutableList<Symbol> body) => new Rule(nonTerminal, body);
+        private ImmutableList<Symbol> RuleBody(Symbol symbol) => ImmutableList<Symbol>.Empty.Add(symbol);
+        private ImmutableList<Symbol> RuleBody(ImmutableList<Symbol> list, Symbol next) => list.Add(next);
+        private Symbol Symbol(Terminal terminal) => terminal;
+        private Symbol Symbol(NonTerminal nonTerminal) => nonTerminal;
 
         private string CompileStringLiteral(string value) => 
             (string)this.StringParser.Parse(value).Compile(this.StringCompiler);
-
-        public object CompileNonTerminal(string label, object[] children) =>
-            label == "F" ? this.CompileFullGrammar(children)
-            : label == "G" ? this.CompileGrammar(children)
-            : label == "R" ? this.CompileRule(children)
-            : label == "B" ? this.CompileBody(children)
-            : label == "S" ? this.CompileSymbol(children)
-            : label == "L" ? this.CompileLexemes(children)
-            : label == "P" ? this.CompileLexemePattern(children)
-            : label == "Q" ? this.CompileString(children)
-            : this.InternalError(label, children);
-
-        private object CompileFullGrammar(object[] children) =>
-            children[0] is ImmutableList<Lexeme> lexemes && children[3] is Grammar grammar ? grammar.AddRange(lexemes)
-            : children[1] is Grammar fullGrammar ? (object)fullGrammar
-            : this.InternalError("Q", children);
-
-        private object CompileGrammar(object[] children) =>
-            children[0] is Rule firstRule ? new Grammar(firstRule)
-            : children[0] is Grammar leftGrammar && children[1] is Rule lastRule ? (object)leftGrammar.Add(lastRule)
-            : this.InternalError("G", children);
-
-        private object CompileRule(object[] children) =>
-            children[0] is NonTerminal nonTerminal && children[2] is ImmutableList<Symbol> symbols ? this.CompileRule(nonTerminal, symbols)
-            : this.InternalError("R", children);
-
-        private object CompileRule(NonTerminal nonTerminal, IEnumerable<Symbol> symbols) =>
-            new Rule(nonTerminal, symbols);
-
-        private object CompileBody(object[] children) =>
-            children[0] is ImmutableList<Symbol> symbols && children[1] is Symbol next ? symbols.Add(next)
-            : children[0] is Symbol first ? (object)ImmutableList<Symbol>.Empty.Add(first)
-            : this.InternalError("B", children);
-
-        private object CompileSymbol(object[] children) =>
-            children[0] is Symbol symbol ? (object)symbol
-            : this.InternalError("S", children);
-
-        private object CompileLexemes(object[] children)
-        {
-            var x = 
-                children.Length == 3 && children[0] is ImmutableList<Lexeme> lexemes && children[1] is Lexeme next ? lexemes.Add(next)
-                : ImmutableList<Lexeme>.Empty;
-            return x;
-        }
-
-        private object CompileLexemePattern(object[] children) =>
-            children.Length == 3 && children[0] is Terminal name && children[2] is Terminal pattern ? new LexemePattern(name.Value, pattern.Value)
-            : children.Length == 2 && children[1] is Terminal ignore ? (object) new IgnoreLexeme(ignore.Value)
-            : this.InternalError("L", children);
-
-        private object CompileString(object[] children) =>
-            string.Join(string.Empty, children.OfType<Terminal>().Select(terminal => terminal.Value).ToArray());
-
-        private string InternalError(string label, object[] children) =>
-            $"Internal error compiling {label} -> {this.ToString(children)}";
-
-        private string ToString(object[] children) =>
-            string.Join(" ", children.Select(child => $"{child}").ToArray());
     }
 }
---
+++ EasyParse/Parsing/MethodMapCompiler.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace EasyParse.Parsing
+{
+    public abstract class MethodMapCompiler : ICompiler
+    {
+        protected abstract IEnumerable<(string label, string methodName)> Map { get; }
+
+        public object CompileTerminal(string label, string value) =>
+            this.Compile(label, value);
+
+        public object CompileNonTerminal(string label, object[] children) =>
+            this.Compile(label, children);
+
+        private object Compile(string label, params object[] children) =>
+            children.OfType<Exception>().FirstOrDefault() is Exception exc ? exc
+            : this.MethodsFor(label, children).FirstOrDefault() is MethodInfo method ? method.Invoke(this, children)
+            : this.Fail(label, children);
+
+        private IEnumerable<MethodInfo> MethodsFor(string label, object[] arguments) =>
+            this.MethodNameFor(label)
+                .SelectMany(methodName => this.GetType()
+                    .GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
+                    .Where(method => method.Name.Equals(methodName)))
+                .Where(method => !method.ContainsGenericParameters)
+                .Where(method => this.CanBind(method, arguments));
+
+        private IEnumerable<string> MethodNameFor(string label) =>
+            this.Map
+                .Where(map => map.label.Equals(label))
+                .Select(map => map.methodName);
+
+        private bool CanBind(MethodInfo method, object[] arguments) =>
+            this.CanBind(method.GetParameters(), arguments);
+
+        private bool CanBind(ParameterInfo[] argumentDefinitions, object[] arguments) =>
+            argumentDefinitions.Length == arguments.Length &&
+            argumentDefinitions.Zip(arguments, (definition, argument) => (definition: definition, value: argument))
+                .All(pair =>
+                    pair.value?.GetType() is Type argumentType && pair.definition.ParameterType.IsAssignableFrom(argumentType)
+                    || pair.value is null && (pair.definition.ParameterType.IsClass || pair.definition.ParameterType.IsInterface));
+
+        private object Fail(string label, IEnumerable<object> arguments) =>
+            new Exception($"Cannot map {label} -> {this.Join(arguments)}");
+
+        private string Join(IEnumerable<object> arguments) =>
+            string.Join(" ", arguments.Select(arg => $"[{this.ToString(arg)}]").ToArray());
+
+        private string ToString(object argument) =>
+            argument is null ? "<null>"
+            : $"{argument.GetType().Name} {argument}";
+    }
+}
