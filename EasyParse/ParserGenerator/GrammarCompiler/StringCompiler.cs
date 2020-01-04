a29e692341672cc9de60746dd922aede58c62369
zoran-horvat <zh@codinghelmet.com>
2020-01-04 17:04:20 +0100

    Preparing ground to support escaping strings

--- EasyParse/ParserGenerator/GrammarCompiler/StringCompiler.cs
+++ EasyParse/ParserGenerator/GrammarCompiler/StringCompiler.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
 using EasyParse.Parsing;
 
 namespace EasyParse.ParserGenerator.GrammarCompiler
@@ -13,15 +15,18 @@ namespace EasyParse.ParserGenerator.GrammarCompiler
 
         private string S(string value) => value;
         private string P(string doubleQuoted) => string.Empty;
-        private string P(string quote1, string quote2) => string.Empty;
-        private string P(string quote1, string content, string quote2) => content;
-        private string C(string value) => value;
-        private string C(string left, string more) => left + more;
+
+        private string P(string quote1, ImmutableList<string> content, string quote2) =>
+            string.Join(string.Empty, content.ToArray());
+
+        private ImmutableList<string> C(string value) => ImmutableList<string>.Empty.Add(value);
+        private ImmutableList<string> C(ImmutableList<string> left, string more) => left.Add(more);
         private string D(string value) => value;
         private string D(string left, string more) => left + more;
         private string G(string value) => value;
         private string V(string at, string doubleQuote) => string.Empty;
-        private string V(string at, string quote1, string quote2) => string.Empty;
-        private string V(string at, string quote1, string content, string quote2) => content;
+
+        private string V(string at, string quote1, ImmutableList<string> content, string quote2) =>
+            string.Join(string.Empty, content.ToArray());
     }
 }
