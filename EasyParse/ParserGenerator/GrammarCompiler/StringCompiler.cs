83ae32888093415f78bacedb4a6dedfe1f1525ca
zoran-horvat <zh@codinghelmet.com>
2020-01-04 12:06:22 +0100

    String compiler now implements MethodMapCompiler

--- EasyParse/ParserGenerator/GrammarCompiler/StringCompiler.cs
+++ EasyParse/ParserGenerator/GrammarCompiler/StringCompiler.cs
@@ -1,25 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
-using EasyParse.ParserGenerator.Models.Symbols;
 using EasyParse.Parsing;
 
 namespace EasyParse.ParserGenerator.GrammarCompiler
 {
-    public class StringCompiler : ICompiler
+    public class StringCompiler : MethodMapCompiler
     {
-        public object CompileTerminal(string label, string value) =>
-            new Terminal(value);
+        protected override IEnumerable<(string terminal, string methodName)> TerminalMap =>
+            Enumerable.Empty<(string, string)>();
 
-        public object CompileNonTerminal(string label, object[] children) =>
-            label == "S" && children.Length == 2 ? string.Empty
-            : label == "S" && children.Length == 3 && children[1] is Terminal finalContent ? finalContent.Value
-            : label == "C" && children.Length == 1 ? children[0]
-            : this.Fail(label, children);
-
-        private object Fail(string label, object[] children) =>
-            new Exception($"{label}({this.Join(children)})");
-
-        private string Join(object[] children) =>
-            string.Join(", ", children.Select(child => $"{child}").ToArray());
+        private string S(string quote1, string quote2) => string.Empty;
+        private string S(string quote1, string content, string quote2) => content;
+        private string C(string value) => value;
     }
 }
