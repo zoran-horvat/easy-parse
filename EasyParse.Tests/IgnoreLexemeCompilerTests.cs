a2283539396dad049b4bb078763834dbe73f7bc3
zoran-horvat <zh@codinghelmet.com>
2020-01-05 16:44:32 +0100

    Changes keyword "is" to "matches" in grammar grammar
    Lexeme patterns are now defined as "<terminal> matches <string>"

--- EasyParse.Tests/IgnoreLexemeCompilerTests.cs
+++ EasyParse.Tests/IgnoreLexemeCompilerTests.cs
@@ -5,7 +5,6 @@ using System.Reflection;
 using EasyParse.LexicalAnalysis;
 using EasyParse.ParserGenerator.GrammarCompiler;
 using EasyParse.ParserGenerator.Models.Rules;
-using EasyParse.Parsing;
 using EasyParse.Testing;
 using Xunit;
 
--- EasyParse/ParserGenerator/GrammarCompiler/GrammarParser.cs
+++ EasyParse/ParserGenerator/GrammarCompiler/GrammarParser.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.Reflection;
-using EasyParse.LexicalAnalysis;
 using EasyParse.ParserGenerator.Models.Rules;
 using EasyParse.Parsing;
 
