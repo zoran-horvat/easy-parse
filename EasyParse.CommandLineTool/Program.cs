46e02c71b839e404b23e5a50e97d6d3be122cbac
zoran-horvat <zh@codinghelmet.com>
2020-01-06 13:23:01 +0100

    Initialized command-line tool project for parsing

---
+++ EasyParse.CommandLineTool/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection;
+
+namespace EasyParse.CommandLineTool
+{
+    class Program
+    {
+        private static string ToolName => Assembly.GetExecutingAssembly().GetName().Name;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ToolName);
+            Console.ReadLine();
+        }
+    }
+}
