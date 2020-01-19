0694ba31af312d01c6c45963d728610f6fe0390d
zoran-horvat <zh@codinghelmet.com>
2020-01-19 23:46:25 +0100

    Introduced multiline text parsing demo
    WordAnalysisDemo project is demonstrating parsing of a multiline input. It simply imputs a series of multiline texts and prints out the longest word in each line of text.

---
+++ EasyParse.WordAnalysisDemo/LongestWordsSelector.cs
@@ -0,0 +1,23 @@
+﻿using EasyParse.Parsing;
+
+namespace EasyParse.WordAnalysisDemo
+{
+    class LongestWordsSelector : MethodMapCompiler
+    {
+        private string TerminalWord(string value) => value;
+
+        private string Text(string lineLongest) => lineLongest;
+
+        private string Text(string textLongest, string lineLongest) =>
+            $"{textLongest}, {lineLongest}";
+
+        private string Line(string endOfLine) => string.Empty;
+
+        private string Line(string longest, string endOfLine) => longest;
+
+        private string Words(string word) => word;
+
+        private string Words(string longest, string next) =>
+            next.Length > longest.Length ? next : longest;
+    }
+}
---
+++ EasyParse.WordAnalysisDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using EasyParse.Parsing;
+
+namespace EasyParse.WordAnalysisDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter blocks of text");
+            Console.WriteLine("End each block with ^Z");
+            Console.WriteLine();
+
+            Parser parser = Parser.FromXmlResource(Assembly.GetExecutingAssembly(), "EasyParse.WordAnalysisDemo.Grammar.xml");
+            ICompiler compiler = new LongestWordsSelector();
+
+            foreach (List<string> text in ReadTexts())
+            {
+                Console.WriteLine(
+                    $"{Environment.NewLine}" +
+                    $"Longest words: {parser.Parse(text).Compile(compiler)}" +
+                    $"{Environment.NewLine}");
+            }
+        }
+
+        static IEnumerable<List<string>> ReadTexts()
+        {
+            while (ReadText().ToList() is List<string> list && list.Any())
+                yield return list;
+        }
+
+        static IEnumerable<string> ReadText()
+        {
+            while (Console.ReadLine() is string line)
+                yield return line;
+        }
+    }
+}
