93b39bec0d74e8533255f15f20decf07832586ec
zoran-horvat <zh@codinghelmet.com>
2020-03-14 21:58:24 +0100

    Official version 1.0.0.1

--- EasyParse/Properties/AssemblyInfo.cs
+++ EasyParse/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ParserCompiler")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("EasyParse")]
+[assembly: AssemblyDescription("Plaintext parser and compiler generator")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyCompany("Coding Helmet")]
 [assembly: AssemblyProduct("ParserCompiler")]
-[assembly: AssemblyCopyright("Copyright © HP Inc. 2019")]
+[assembly: AssemblyCopyright("Copyright © 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
