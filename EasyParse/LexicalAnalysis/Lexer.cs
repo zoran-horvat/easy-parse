8f1ba5e201c08ec340087dbe3d6ec84299d196c6
zoran-horvat <zh@codinghelmet.com>
2020-10-04 15:48:42 +0200

    Supported multiple target frameworks
    EasyParse library now supports .NET Framework 4.7.2, .NET Standard 2.0 and .NET Core 3.0

--- EasyParse/LexicalAnalysis/Lexer.cs
+++ EasyParse/LexicalAnalysis/Lexer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
+using System.Text.RegularExpressions;
 using EasyParse.LexicalAnalysis.Tokens;
 using EasyParse.Text;
 
