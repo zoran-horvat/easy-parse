32c5558705cf4d482073546e78a9615c1ec81939
Zoran Horvat <zh@codinghelmet.com>
2021-10-10 17:52:00 +0200

    Completed arithmetic demo with fluent API

--- EasyParse.CalculatorDemo/ArithmeticGrammar.cs
+++ EasyParse.CalculatorDemo/ArithmeticGrammar.cs
@@ -7,17 +7,32 @@ namespace EasyParse.CalculatorDemo
 {
     class ArithmeticGrammar : Grammar
     {
-        public IRule Value() => Rule()
+        public IRule Unit() => Rule()
             .Regex("number", @"\d+", int.Parse).ToIdentity<int>()
+            .Literal("-").Symbol(Unit).To((string _, int value) => -value)
             .Literal("(").Symbol(Additive).Literal(")").To((string _, int additive, string _) => additive);
 
         public IRule Multiplicative() => Rule()
-            .Symbol(Value).ToIdentity<int>()
-            .Symbol(Multiplicative).Literal("*").Symbol(Value).To((int a, string _, int b) => a * b);
+            .Symbol(Unit).ToIdentity<int>()
+            .Symbol(Multiply).ToIdentity<int>()
+            .Symbol(Divide).ToIdentity<int>();
+
+        public IRule Multiply() => Rule()
+            .Symbol(Multiplicative).Literal("*").Symbol(Unit).To((int a, string _, int b) => a * b);
+
+        public IRule Divide() => Rule()
+            .Symbol(Multiplicative).Literal("/").Symbol(Unit).To((int a, string _, int b) => a / b);
 
         public IRule Additive() => Rule()
             .Symbol(Multiplicative).ToIdentity<int>()
+            .Symbol(Add).ToIdentity<int>()
+            .Symbol(Subtract).ToIdentity<int>();
+
+        public IRule Add() => Rule()
             .Symbol(Additive).Literal("+").Symbol(Multiplicative).To((int a, string _, int b) => a + b);
+
+        public IRule Subtract() => Rule()
+            .Symbol(Additive).Literal("-").Symbol(Multiplicative).To((int a, string _, int b) => a - b);
 
         public IRule Expression() => Rule().Symbol(Additive).ToIdentity<int>();
 
