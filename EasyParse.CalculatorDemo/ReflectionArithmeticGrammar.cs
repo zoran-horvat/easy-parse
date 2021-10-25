6440471055db46ff2fa9357ea04d351c82d8276d
Zoran Horvat <zh@codinghelmet.com>
2021-10-25 02:12:19 +0200

    Improved readability of sample reflection-based grammar

--- EasyParse.CalculatorDemo/ReflectionArithmeticGrammar.cs
+++ EasyParse.CalculatorDemo/ReflectionArithmeticGrammar.cs
@@ -6,22 +6,34 @@ namespace EasyParse.CalculatorDemo
 {
     class ReflectionArithmeticGrammar : ReflectionGrammar
     {
-        protected override IEnumerable<Regex> IgnorePatterns => new[] {new Regex(@"\s+")};
+        protected override IEnumerable<Regex> IgnorePatterns => 
+            new[] {new Regex(@"\s+")};
 
-        [NonTerminal] protected int Number([Regex("number", @"\d+")] string number) => int.Parse(number);
+        [NonTerminal] protected int Number(
+            [Regex("number", @"\d+")] string number) => int.Parse(number);
 
-        [NonTerminal] protected int Unit([From(nameof(Number))] int number) => number;
-        [NonTerminal] protected int Unit([Literal("-")] string minus, [From(nameof(Number))] int number) => -number;
-        [NonTerminal] protected int Unit([Literal("(")] string open, [From(nameof(Additive))] int value, [Literal(")")] string close) => value;
+        [NonTerminal] protected int Unit(
+            [From(nameof(Number))] int number) => number;
+        [NonTerminal] protected int Unit(
+            [Literal("-")] string minus, [From(nameof(Number))] int number) => -number;
+        [NonTerminal] protected int Unit(
+            [Literal("(")] string open, [From(nameof(Additive))] int value, [Literal(")")] string close) => value;
 
-        [NonTerminal] protected int Additive([From(nameof(Multiplicative), nameof(Added), nameof(Subtracted))] int value) => value;
-        [NonTerminal] protected int Added([From(nameof(Additive))] int a, [Literal("+")] string plus, [From(nameof(Multiplicative))] int b) => a + b;
-        [NonTerminal] protected int Subtracted([From(nameof(Additive))] int a, [Literal("-")] string minus, [From(nameof(Multiplicative))] int b) => a - b;
+        [NonTerminal] protected int Additive(
+            [From(nameof(Multiplicative), nameof(Added), nameof(Subtracted))] int value) => value;
+        [NonTerminal] protected int Added(
+            [From(nameof(Additive))] int a, [Literal("+")] string plus, [From(nameof(Multiplicative))] int b) => a + b;
+        [NonTerminal] protected int Subtracted([
+            From(nameof(Additive))] int a, [Literal("-")] string minus, [From(nameof(Multiplicative))] int b) => a - b;
 
-        [NonTerminal] protected int Multiplicative([From(nameof(Unit), nameof(Multiplied), nameof(Divided))] int value) => value;
-        [NonTerminal] protected int Multiplied([From(nameof(Multiplicative))] int a, [Literal("*")] string star, [From(nameof(Unit))] int b) => a * b;
-        [NonTerminal] protected int Divided([From(nameof(Multiplicative))] int a, [Literal("/")] string slash, [From(nameof(Unit))] int b) => a / b;
+        [NonTerminal] protected int Multiplicative(
+            [From(nameof(Unit), nameof(Multiplied), nameof(Divided))] int value) => value;
+        [NonTerminal] protected int Multiplied(
+            [From(nameof(Multiplicative))] int a, [Literal("*")] string star, [From(nameof(Unit))] int b) => a * b;
+        [NonTerminal] protected int Divided(
+            [From(nameof(Multiplicative))] int a, [Literal("/")] string slash, [From(nameof(Unit))] int b) => a / b;
 
-        [Start] [NonTerminal] protected int Expression([From(nameof(Additive))] int value) => value;
+        [Start] [NonTerminal] protected int Expression(
+            [From(nameof(Additive))] int value) => value;
     }
 }
